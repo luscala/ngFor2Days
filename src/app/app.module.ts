31c30a5a04cdcfe9916bd22a790db65a131f58ca
luscala <lucascala05@gmail.com>
2021-04-01 07:13:19 +0200

    3 files modified, 2 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,31 @@ import { AppComponent } from "./app.component";
 import { HelloPipe } from "./hello.pipe";
 import { TrimDirective } from "./trim.directive";
 import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { Route1Component } from "./route1.component";
+import { Route2Component } from "./route2.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, HelloPipe, TrimDirective],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: "route1",
+        component: Route1Component
+      },
+      {
+        path: "route2",
+        component: Route2Component
+      },
+      {
+        path: "**",
+        redirectTo: "route1"
+      }
+    ])
+  ],
+  declarations: [AppComponent, HelloPipe, TrimDirective, Route1Component],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/route1.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-route1",
+  template: `
+    Hello World! I'm route1
+  `
+})
+export class Route1Component {}
